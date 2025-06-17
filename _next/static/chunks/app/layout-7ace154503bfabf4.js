ac519fadce0ee507d1c5c3af242cb3d3a2978633
Sohrab Alam <34127382+md-sohrab-alam@users.noreply.github.com>
2025-06-17 14:47:04 +0300

    Updates

--- _next/static/chunks/app/layout-7ace154503bfabf4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},2975:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_e8ce0c"}},8483:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,2975,23)),Promise.resolve().then(l.t.bind(l,347,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[315,690,441,684,358],()=>s(8483)),_N_E=e.O()}]);
