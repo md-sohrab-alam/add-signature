1991b33cb400135b929a4268ef1f0a95ec3e6479
Sohrab Alam <34127382+md-sohrab-alam@users.noreply.github.com>
2025-06-17 15:39:03 +0300

    Update react-signature-canvas type definitions to include getTrimmedCanvas and other methods

--- src/types/react-signature-canvas.d.ts
+++ src/types/react-signature-canvas.d.ts
@@ -8,12 +8,19 @@ declare module 'react-signature-canvas' {
     maxWidth?: number;
     penColor?: string;
     velocityFilterWeight?: number;
+    dotSize?: number;
+    minDistance?: number;
   }
 
   export default class SignatureCanvas extends React.Component<SignatureCanvasProps> {
     clear(): void;
     isEmpty(): boolean;
-    toDataURL(type?: string): string;
+    toDataURL(type?: string, encoderOptions?: number): string;
     fromDataURL(dataURL: string): void;
+    getTrimmedCanvas(): HTMLCanvasElement;
+    getCanvas(): HTMLCanvasElement;
+    getPoints(): Array<any>;
+    off(): void;
+    on(): void;
   }
 } 
